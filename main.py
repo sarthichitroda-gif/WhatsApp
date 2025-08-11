91ba57d3ac6e4dbc0854fd1c8ed4489666a72ddc
Sarthi Chitroda <sarthichitroda@gmail.com>
2025-08-11 15:27:38 +0530

    added error message for wrong linkedin profile format in get person

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ async def webhook(request: Request):
                 )
             else:
                 fulfillment_text = (
-                    f"There was an error fetching the details of the user. Please try again later."
+                    f"return None, Please make sure the input is in proper format https://www.linkedin.com/in/userid"
                 )
 
         elif intent == "GetPersonalityAnalysis":
